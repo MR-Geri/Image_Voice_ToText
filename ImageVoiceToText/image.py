5ee08b080297f0bd2de4f2be4b4d9b1a2c724322
mrgeri <ilya.kamckine@ya.ru>
2021-06-04 13:56:53 +0300

    fix

--- ImageVoiceToText/image.py
+++ ImageVoiceToText/image.py
@@ -10,7 +10,7 @@ config = r'--oem 3 --psm 6'
 def image_to_text(path: str) -> Union[None, str]:
     try:
         image = cv2.imread(path)
-        image = imutils.resize(image, width=int(1.5 * len(image[0])))
+        image = imutils.resize(image, width=max(600, int(1.5 * len(image[0]))))
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
